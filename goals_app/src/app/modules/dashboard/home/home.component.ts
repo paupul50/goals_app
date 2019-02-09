24f93bfa16bf33da73a4df54e7f7117e6cc213d0
Paulius Puleikis <33981391+paupul50@users.noreply.github.com>
2019-02-09 19:55:09 +0200

    Added http module and configured android config for localhost testing.

--- goals_app/src/app/modules/dashboard/home/home.component.ts
+++ goals_app/src/app/modules/dashboard/home/home.component.ts
@@ -1,17 +1,30 @@
 import { Component, OnInit } from "@angular/core";
-
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 @Component({
     selector: "Home",
     moduleId: module.id,
     templateUrl: "./home.component.html"
 })
 export class HomeComponent implements OnInit {
+    //temp
+    tempData: string[];
+    europianCountries = ["pirmas", "antras"];
+    private _headers = new HttpHeaders({
+        "Content-Type": "application/json",
+    });
 
-    constructor() {
-        // Use the component constructor to inject providers.
+    constructor(private http: HttpClient) {
+
     }
 
     ngOnInit(): void {
         // Init your component properties here.
+    }
+    //temp
+    onTap(): void {
+        this.http.get('http://localhost:52503/api/values', { headers: this._headers }).subscribe((response: string[]) => {
+            this.tempData = response;
+        });
+        console.log(this.tempData);
     }
 }
