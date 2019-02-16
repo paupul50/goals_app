c77cc818dc7cc647f4a4f6304a36f006f71c6329
Paulius Puleikis <33981391+paupul50@users.noreply.github.com>
2019-02-16 20:59:29 +0200

    Bluetooth integration and a test with mi band 3 peripheral.

--- goals_app/src/app/app-routing.module.ts
+++ goals_app/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes } from "@angular/router";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 
 const routes: Routes = [
-    { path: "", redirectTo: "/home", pathMatch: "full" },
+    { path: "", redirectTo: "tracker", pathMatch: "full" },
     { path: "home", loadChildren: "~/app/modules/dashboard/dashboard.module#DashboardModule"},
     { path: "tracker", loadChildren: "~/app/modules/tracker/tracker.module#TrackerModule"}
 ];
--- goals_app/src/app/modules/dashboard/components/home/home.component.ts
+++ goals_app/src/app/modules/dashboard/components/home/home.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 
-// import * as Bluetooth from 'nativescript-bluetooth';
-
 @Component({
     selector: "Home",
     moduleId: module.id,
